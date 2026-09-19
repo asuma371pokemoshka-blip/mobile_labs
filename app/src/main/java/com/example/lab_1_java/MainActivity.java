81e35de48ae5daf802a181b93926ac3845c19440
akitam <273786810+asuma371pokemoshka-blip@users.noreply.github.com>
2026-09-19 18:29:12 +0600

    Attach BlankFragment to container in MainActivity

--- app/src/main/java/com/example/lab_1_java/MainActivity.java
+++ app/src/main/java/com/example/lab_1_java/MainActivity.java
@@ -2,23 +2,31 @@ package com.example.lab_1_java;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // savedInstanceState == null означает, что активность создаётся впервые
+        // (а не пересоздаётся, например, при повороте экрана).
+        // Это защита от дублирования фрагментов.
+        if (savedInstanceState == null) {
+
+            BlankFragment blankFragment = new BlankFragment();
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            transaction.replace(R.id.fragment_container, blankFragment);
+
+            transaction.commit();
+        }
     }
 }
